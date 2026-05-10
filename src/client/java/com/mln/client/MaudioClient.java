85898af82f13cad46779c0acb909ea5fb8f8d46e
PorchugaWhale <75288345+MatthewWeyer@users.noreply.github.com>
2026-05-10 12:59:11 -0400

    added command

--- src/client/java/com/mln/client/MaudioClient.java
+++ src/client/java/com/mln/client/MaudioClient.java
@@ -1,10 +1,20 @@
 package com.mln.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.network.chat.Component;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 
 public class MaudioClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// This entrypoint is suitable for setting up client-specific logic, such as
+		// rendering.
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("clienttater").executes(context -> {
+				context.getSource().sendFeedback(Component.literal("Called /clienttater with no arguments."));
+				return 1;
+			}));
+		});
 	}
 }
