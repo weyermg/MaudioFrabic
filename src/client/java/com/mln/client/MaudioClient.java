58af89fcade69224d0e87b8b4dc45ffc74f830a7
PorchugaWhale <75288345+MatthewWeyer@users.noreply.github.com>
2026-05-10 12:24:26 -0400

    Please dont break anything no promises

---
+++ src/client/java/com/mln/client/MaudioClient.java
@@ -0,0 +1,10 @@
+package com.mln.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaudioClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
